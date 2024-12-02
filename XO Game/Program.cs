2e974c2569e1130a3022c56b3895c812f03acac6
ahmed hamzaoui <ahmedhamzaoui2003@gmail.com>
2024-12-02 16:26:26 +0100

    Finish The Game

--- XO Game/Program.cs
+++ XO Game/Program.cs
@@ -32,9 +32,153 @@ namespace XO_Game
 
         }
 
+        static int ReadPlayerTurnPosition(string msg)
+        {
+            Console.WriteLine(msg);
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void PutPlayerChoiceInXOGrid(int Position,char choice)
+        {
+            switch (Position)
+            {
+                case 1:
+                    {
+                        XOGrid[0][0] = choice;
+                        break;
+                    }
+                case 2:
+                    {
+                            XOGrid[0][1] = choice;
+                            break;
+                    }
+                case 3:
+                    {
+                            XOGrid[0][2] = choice;
+                            break;
+                    }
+                case 4:
+                    {
+                        XOGrid[1][0] = choice;
+                        break;
+                    }
+
+                case 5:
+                    {
+                        XOGrid[1][1] = choice;
+                        break;
+                    }
+                case 6:
+                    {
+                        XOGrid[1][2] = choice;
+                        break;
+                    }
+                case 7:
+                    {
+                        XOGrid[2][0] = choice;
+                        break;
+                    }
+                case 8:
+                    {
+                        XOGrid[2][1] = choice;
+                        break;
+                    }
+                case 9:
+                    {
+                        XOGrid[2][2] = choice;
+                        break;
+                    }
+                default:
+                    Console.WriteLine("\a");
+                    break;
+
+            }
+        }
+        
+        static bool DiagonalWin(char XOchoice)
+        {
+            return (XOGrid[0][0] == XOchoice && XOGrid[1][1] == XOchoice && XOGrid[2][2] == XOchoice) || (XOGrid[0][2] == XOchoice && XOGrid[1][1] == XOchoice && XOGrid[2][0] == XOchoice);
+        }
+
+        static bool HorizontalWin(char XOchoice)
+        {
+            return (XOGrid[0][0] == XOchoice && XOGrid[0][1] == XOchoice && XOGrid[0][2] == XOchoice) || (XOGrid[1][0] == XOchoice && XOGrid[1][1] == XOchoice && XOGrid[1][2] == XOchoice) || (XOGrid[2][0] == XOchoice && XOGrid[2][1] == XOchoice && XOGrid[2][2] == XOchoice);
+        }
+
+        static bool VerticalWin(char XOchoice)
+        {
+            return (XOGrid[0][0] == XOchoice && XOGrid[1][0] == XOchoice && XOGrid[2][0] == XOchoice) || (XOGrid[0][1] == XOchoice && XOGrid[1][1] == XOchoice && XOGrid[2][1] == XOchoice) || (XOGrid[0][2] == XOchoice && XOGrid[1][2] == XOchoice && XOGrid[2][2] == XOchoice);
+        }
+
+        static bool isWinner(char choice)
+        {
+            return VerticalWin(choice) || HorizontalWin(choice) || DiagonalWin(choice);
+        }
+
+        static bool isDraw()
+        {
+            foreach(List<char> row in XOGrid)
+            {
+                foreach(char c in row)
+                {
+                    if (char.IsDigit(c)) return false;
+                }
+            }
+            return true;
+        }
+        
+        public static void StartXOGame()
+        {
+            int Counter = 0;
+            while(true)
+            {
+                Counter++;
+                ShowX0Grid();
+                
+                if (isDraw())
+                {
+                    Console.WriteLine("No Winner in this Game");
+                    break;
+                }
+
+                if(Counter % 2 != 0)
+                {
+                    PutPlayerChoiceInXOGrid(ReadPlayerTurnPosition("Ahmed turn!"),Player1Choice);
+                    
+                    if(isWinner(Player1Choice))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Ahmed is the Winner in this Game");
+                        Console.WriteLine("Mokhtar Ya 5obzti hhhh :-)");
+                        break;
+                    }
+                }else
+                 {
+                    
+                    PutPlayerChoiceInXOGrid(ReadPlayerTurnPosition("Mokhtar turn!"), Player2Choice);
+                    
+                    if (isWinner(Player2Choice))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.BackgroundColor = ConsoleColor.Green; 
+                        Console.WriteLine("Mokhtar is the Winner in this Game");
+                        break;
+                    }
+                }
+
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("I wish you have fun with my XO Game");
+        }
+        
+        
         static void Main(string[] args)
         {
-            ShowX0Grid();
+            StartXOGame();
+
+
             Console.ReadKey();
         }
     }
